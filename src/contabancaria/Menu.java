ae7cbb1aa13513772ffd99cb6ceb874026a1eb10
Tomas Souza de Jesus <tomassouzapasquinha@outlook.com>
2025-04-24 21:19:18 -0300

    01-menu

---
+++ src/contabancaria/Menu.java
@@ -0,0 +1,77 @@
+package contabancaria;
+
+import contabancaria.util.Cores;
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+        int opcao;
+
+        while (true) {
+           
+            System.out.println(Cores.TEXT_PURPLE_BOLD + Cores.ANSI_GREEN_BACKGROUND +
+                    "============================================\n" +
+                    "             CONTA BANCÁRIA                 \n" +
+                    "============================================\n" +
+                    "1 - Criar Conta\n" +
+                    "2 - Listar todas as Contas\n" +
+                    "3 - Buscar Conta por Número\n" +
+                    "4 - Atualizar Dados da Conta\n" +
+                    "5 - Apagar Conta\n" +
+                    "6 - Sacar\n" +
+                    "7 - Depositar\n" +
+                    "8 - Transferir valores entre Contas\n" +
+                    "0 - Sair\n" +
+                    "============================================" +
+                    Cores.TEXT_RESET);
+            
+            System.out.print("Entre com a opção desejada: ");
+            
+            try {
+                opcao = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println(Cores.TEXT_RED + "Por favor, digite apenas números válidos!" + Cores.TEXT_RESET);
+                continue;
+            }
+
+            if (opcao == 0) {
+                System.out.println(Cores.TEXT_GREEN_BOLD + "Aplicação finalizada com sucesso!" + Cores.TEXT_RESET);
+                scanner.close();
+                System.exit(0);
+            }
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Você escolheu: Criar Conta");
+                    break;
+                case 2:
+                    System.out.println("Você escolheu: Listar todas as Contas");
+                    break;
+                case 3:
+                    System.out.println("Você escolheu: Buscar Conta por Número");
+                    break;
+                case 4:
+                    System.out.println("Você escolheu: Atualizar Dados da Conta");
+                    break;
+                case 5:
+                    System.out.println("Você escolheu: Apagar Conta");
+                    break;
+                case 6:
+                    System.out.println("Você escolheu: Sacar");
+                    break;
+                case 7:
+                    System.out.println("Você escolheu: Depositar");
+                    break;
+                case 8:
+                    System.out.println("Você escolheu: Transferir valores entre Contas");
+                    break;
+                default:
+                    System.out.println(Cores.TEXT_RED + "Opção inválida. Tente novamente." + Cores.TEXT_RESET);
+                    break;
+            }
+        }
+    }
+}
